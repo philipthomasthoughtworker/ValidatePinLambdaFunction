28e24a1b966ccbf6bf16d8bbfc3950e65ec44327
philipthomasthoughtworker <philip.thomas@thoguhtworks.com>
2019-02-11 13:32:51 -0500

    Commit by Philip Thomas

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-exports.handler = async (event, context) => {
+exports.handler = async (event) => {
     const aws = require('aws-sdk');
     const s3 = new aws.S3();
     const params = { Bucket: 'personalidentificationnumbers', Key : 'pin1.txt' };
     const response = await s3.getObject(params).promise();
     const isMatch = response.Body.toString('utf-8').match("^\\d{4,6}$");
     
-    console.log('webpage = ' + event.webpage);
-    context.done(null, 'link-scrapper complete.');
-    console.log('isMatch' + isMatch);
-
     if (isMatch === null)
         return { statusCode : 200, body : JSON.stringify({'isMatch' : 'false' }) };
     else
